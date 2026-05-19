8f505c92f8849d1a86e04f5b36dab1b77d0c9d7c
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-18 23:36:02 -0400

    feat: wire movie route handlers to controller imports

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,2 +1,5 @@
 const express = require('express');
 const router = express.Router();
+
+//Imported 2 Controller Functions
+const { searchMovies, getMovieDetails } = require('../controllers/movieController.js');
