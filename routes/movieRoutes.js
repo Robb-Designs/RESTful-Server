0bcb13b8aab9379463bdaac0f0951025f2b0b9e2
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-18 23:48:17 -0400

    feat: add movie search and details routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); //empty router obj
 
 //Imported 2 Controller Functions
 const { searchMovies, getMovieDetails } = require('../controllers/movieController.js');
+
+
+
+
+// ADDING ROUTES TO EMPTY ROUTER OBJ VARIABLE ABOVE
+//search route
+router.get('/search', searchMovies);
+
+//Movie Details Route
+router.get('/movies/:id', getMovieDetails);
+
+module.exports = router;
